0efae1554fc80468c593db8bb3d419e72172a5f9
johannvonneumann <juyeun9086@gmail.com>
2021-12-01 15:42:44 +0900

    first commit

--- job01.py
+++ job01.py
@@ -1,1 +1,9 @@
 print('git test')
+#crawling작업
+#crawling 각자진행, 빨리 완성되는코드로 연도나눠 진행
+#일단2020 개봉작만 크롤링 , 저장형식은 CSV
+#나머지는 연도별로 나눠 크롤링 해서 합
+#컬럼명 ['title', 'reviews']
+#파일명 review_0000.csv (0000은해당연도)
+#링크주소
+#https://drive.google.com/drive/folders/1vICarPIiRxa65pItqwBEwvMD_tlf0xQw?usp=sharing
